26cc8b6f2b99b519c3e97ad5073c77b52ae67165
PhoduCoder <gauravprashar17@gmail.com>
2018-02-21 23:50:58 +0530

    Create Prob1.py
    A file that prints all numbers between 2000 and 3200 which are divisible by 7 but not by 5

---
+++ Prob1.py
@@ -0,0 +1,16 @@
+#print all numbers from 2000 to 3200
+
+num_list = []
+
+num_list2 = []
+
+for i in range(2000, 3201):
+    num_list.append(i)
+    if ((i%7)==0 and (i%5)!=0):
+        num_list2.append(i)
+        print i,
+
+#print num_list.len()
+
+print len(num_list2)
+print len(num_list)
