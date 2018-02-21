81fce876257df58c425702fde0afdfa96876f91d
PhoduCoder <gauravprashar17@gmail.com>
2018-02-22 00:02:43 +0530

    Update Prob1.py
    Made appropriate changes

--- Prob1.py
+++ Prob1.py
@@ -7,10 +7,16 @@ num_list2 = []
 for i in range(2000, 3201):
     num_list.append(i)
     if ((i%7)==0 and (i%5)!=0):
-        num_list2.append(i)
+        #num_list2.append(i)
+        num_list2.append(str(i))
         print i,
 
 #print num_list.len()
 
 print len(num_list2)
 print len(num_list)
+
+#print ','.join(num_list2)
+# This will through a error 
+# TypeError: sequence item 0: expected string, int found
+print ','.join(num_list2)
