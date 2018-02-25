008733928eb8328482311a50cefce23497c65c74
PhoduCoder <gauravprashar17@gmail.com>
2018-02-25 13:16:35 +0530

    Updated PrintMatrix
    Added one more method to print matrix in string format

--- PrintMatrix.py
+++ PrintMatrix.py
@@ -3,7 +3,7 @@ my_matrix=[[2,3],[100,200]]
 my_sec_matrix=[[4,5],[7,9],[100,200]]
 
 #Print the matrix
-
+#This will print the numbers
 def print_matrix(m):
     for i in m:
         for j in i:
@@ -13,3 +13,13 @@ def print_matrix(m):
 print_matrix(my_matrix)
 
 print_matrix(my_sec_matrix)
+
+def print_matrix_nicely(m):
+    ln=""
+    for i in m:
+        for j in i:
+            ln += str(j) + " "
+        ln = ln +"\n"
+    print (ln)
+
+print_matrix_nicely(my_sec_matrix)
